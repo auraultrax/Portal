6f7483ab1a9dd78ba83a02c4402e25a93d71af4e
auraultrax <xauraultra@gmail.com>
2026-09-09 13:05:42 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='beray-hub-v4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/beray-icon-512.png','./icons/aurax-icon-512.png','./icons/saglik-icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.origin!==location.origin)return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
-});
